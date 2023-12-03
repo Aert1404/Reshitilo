02df05ecfd847fc132b180dc4c48082b691c2fd2
Vlad1k822 <vladblade2009@gmail.com>
2023-12-03 10:03:48 +0200

    create turtle25

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from turtle import *
+import time
+shape("turtle")
+name1=int(input('Enter width ->'))
+name2=int(input('Enter hight ->'))
+color("blue", "yellow")
+forward(name1)
+left(90)
+forward(name2)
+left(90)
+forward(name1)
+left(90)
+forward(name2)
+left(90)
+forward(name1)
+time.sleep(10)
